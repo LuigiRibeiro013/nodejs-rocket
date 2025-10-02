b64d4ab124123b8c94f74948a768c6471878b3c4
Luigi Ribeiro <luigiribeiro22@gmail.com>
2025-10-02 16:43:41 -0300

    streams

--- streams/fundamentals.js
+++ streams/fundamentals.js
@@ -1,7 +1,7 @@
 // process.stdin
 //     .pipe(process.stdout)
 
-import { Readable, Writable } from "node:stream";
+import { Readable, Writable, Transform } from "node:stream";
 
 class OneToHundredStream extends Readable {
   index = 1;
@@ -21,6 +21,14 @@ class OneToHundredStream extends Readable {
   }
 }
 
+class InverseNumberStream extends Transform {
+  _transform(chunk, encoding, callback) {
+    const transformed = Number(chunk.toString()) * -1;
+
+    callback(null, Buffer.from(String(transformed)));
+  }
+}
+
 class MultiplyByTenStream extends Writable {
   _write(chunk, encoding, callback) {
     console.log(Number(chunk.toString()) * 10);
@@ -28,4 +36,6 @@ class MultiplyByTenStream extends Writable {
   }
 }
 
-new OneToHundredStream().pipe(new MultiplyByTenStream());
+new OneToHundredStream()
+  .pipe(new InverseNumberStream())
+  .pipe(new MultiplyByTenStream());

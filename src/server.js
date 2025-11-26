516d5279fc756c99635eb4a7ec45e0072196ef12
Luigi Ribeiro <luigiribeiro22@gmail.com>
2025-11-26 15:24:05 -0300

    prettier

--- src/server.js
+++ src/server.js
@@ -5,26 +5,26 @@ import { routes } from "./routes.js";
 import { extractQueryParams } from "./utils/extract-query-params.js";
 
 const server = http.createServer(async (req, res) => {
-  const { method, url } = req;
+    const { method, url } = req;
 
-  await json(req, res);
+    await json(req, res);
 
-  const route = routes.find((route) => {
-    return route.method === method && route.path.test(url);
-  });
+    const route = routes.find((route) => {
+        return route.method === method && route.path.test(url);
+    });
 
-  if (route) {
-    const routeParams = req.url.match(route.path);
+    if (route) {
+        const routeParams = req.url.match(route.path);
 
-    const { query, ...params } = routeParams.groups;
+        const { query, ...params } = routeParams.groups;
 
-    req.params = params;
-    req.query = query ? extractQueryParams(query) : {};
+        req.params = params;
+        req.query = query ? extractQueryParams(query) : {};
 
-    return route.handler(req, res);
-  }
+        return route.handler(req, res);
+    }
 
-  return res.writeHead(404).end();
+    return res.writeHead(404).end();
 });
 
 server.listen(3333);
